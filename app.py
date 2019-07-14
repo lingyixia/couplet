4e317ce3acadcf6527c8a2071a3b53725bb33327
chenfeiyu01 <chenfeiyu01@chenfeiyu.local>
2019-07-14 16:13:06 +0800

    web

--- app.py
+++ app.py
@@ -8,7 +8,7 @@
 # @DateTime:     2019-06-26 17:02
 # @Contact       chenfeiyu01@baidu.com
 # @Version       1.0
-# @Description:  
+# @Description:
 # -------------------------------------------------------------------------------
 from pathlib import Path
 import tensorflow as tf
@@ -17,6 +17,9 @@ import argparse, os, json
 import numpy as np
 from dataHelper import DataHelper
 from Model import Couplet
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)  # 实例化app对象
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
@@ -34,9 +37,11 @@ parser.add_argument('--batch_size', type=int, default=32, help='batchSize')
 parser.add_argument('--num_epoch', type=int, default=50, help='epoches')
 parser.add_argument('--max_length', type=int, default=100, help='序列最大长度')
 parser.add_argument('--model_path', type=str, default='model', help='模型保存路径')
-parser.add_argument('--beam_search', type=bool, default=False, help='是否使用BeamSearch')
+parser.add_argument('--beam_search', type=bool, default=True, help='是否使用BeamSearch')
 parser.add_argument('--beam_size', type=bool, default=5, help='beam_size')
 parser.add_argument('--encode_layer_size', type=bool, default=6, help='编码层bisltm层数')
+
+FLAGS = parser.parse_args()
 
 
 def model_fn(features, labels, mode, params):
@@ -62,7 +67,6 @@ def model_fn(features, labels, mode, params):
                     layer_size=params['encodeLayerSize'])
     if mode == tf.estimator.ModeKeys.TRAIN:
         loss, train_op, input, output = model.getResult(mode)
-        train_logging_hook = tf.train.LoggingTensorHook({"input": input, 'output': output}, every_n_iter=100)
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
     elif mode == tf.estimator.ModeKeys.EVAL:
         loss = model.getResult(mode)
@@ -72,23 +76,15 @@ def model_fn(features, labels, mode, params):
         return tf.estimator.EstimatorSpec(mode=mode, predictions=sequence)
 
 
-if __name__ == '__main__':
-    tf.logging.set_verbosity(tf.logging.INFO)
-    # np.set_printoptions(threshold=np.inf)
-    FLAGS = parser.parse_args()
-    # os.chdir('/content/drive/ColaboratoryLab/couplet')
-    cfg = tf.estimator.RunConfig(save_checkpoints_secs=120, keep_checkpoint_max=5)
-    if not Path(FLAGS.model_path).exists():
-        Path(FLAGS.model_path).mkdir()
-    with Path(FLAGS.model_path).joinpath('params').open(mode='w') as writer:
-        json.dump(vars(FLAGS), writer)
-    dataHelper = DataHelper(os.path.join(FLAGS.dataPath, 'vocabs'))
-    params = {'hiddenSize': FLAGS.hidden_size,
-              'embeddingSize': FLAGS.embedding_size,
-              'dropout': FLAGS.dropout,
-              'l2Regularizer': FLAGS.l2_regularizer,
-              'baseLearnRate': FLAGS.base_learnRate,
-              'maxLength': FLAGS.max_length,
+dataHelper = DataHelper(os.path.join(FLAGS.dataPath, 'vocabs'))
+with open('model/params') as reader:
+    params = json.load(reader)
+    params = {'hiddenSize': params["hidden_size"],
+              'embeddingSize': params["embedding_size"],
+              'dropout': params["dropout"],
+              'l2Regularizer': params["l2_regularizer"],
+              'baseLearnRate': params["base_learnRate"],
+              'maxLength': params["max_length"],
               'startToken': dataHelper.vocab2index['<s>'],
               'endToken': dataHelper.vocab2index['</s>'],
               'vocabs': len(dataHelper.vocab2index),
@@ -96,24 +92,39 @@ if __name__ == '__main__':
               'beamSize': FLAGS.beam_size,
               'encodeLayerSize': FLAGS.encode_layer_size
               }
-    model = tf.estimator.Estimator(model_fn=model_fn, model_dir='model', config=cfg, params=params)
-    train_inputFun = functools.partial(dataHelper.input_fn, os.path.join(FLAGS.dataPath, 'train', 'in.txt'),
-                                       os.path.join(FLAGS.dataPath, 'train', 'out.txt'),
-                                       epoch_num=50)
-    dev_inputFun = functools.partial(dataHelper.input_fn, os.path.join(FLAGS.dataPath, 'dev', 'in.txt'),
-                                     os.path.join(FLAGS.dataPath, 'dev', 'out.txt'), is_shuffle_and_repeat=False)
-    #     train_spec = tf.estimator.TrainSpec(input_fn=train_inputFun)
-    #     model.train(train_inputFun)
-    eval_spec = tf.estimator.EvalSpec(input_fn=dev_inputFun, throttle_secs=120)
-    #    tf.estimator.train_and_evaluate(model, train_spec, eval_spec)
-    model.evaluate(dev_inputFun)
-    test_inputFun = functools.partial(dataHelper.input_fn, os.path.join(FLAGS.dataPath, 'test', 'in.txt'),
-                                      os.path.join(FLAGS.dataPath, 'test', 'out.txt'), is_shuffle_and_repeat=False)
+model = tf.estimator.Estimator(model_fn=model_fn, model_dir='model', params=params)
+
+
+@app.route('/send', methods=['GET', 'POST'])  # 路由
+def test_post():
+    uplink = request.form.get('uplink')
+    uplink = ' '.join(list(uplink)) + '\n'
+    with open('./temp.txt', mode='w') as writer:
+        writer.write(uplink)
+    test_inputFun = functools.partial(dataHelper.input_fn, os.path.join('.', 'temp.txt'),
+                                      os.path.join('.', 'temp.txt'), batch_size=1, is_shuffle_and_repeat=False)
     predictions = model.predict(test_inputFun)
+    down_links = list()
     for result in predictions:
         print(dataHelper.dataTransform(result['up_link']))
         if FLAGS.beam_search:
             for line in range(FLAGS.beam_size):
                 print(dataHelper.dataTransform(result['down_link'][line]))
+                down_links.append(dataHelper.dataTransform(result['down_link'][line]))
         else:
             print(dataHelper.dataTransform(result['down_link']))
+    links = {'uplink': "上联:  " + ''.join(uplink.split()), 'downlink': down_links}
+    return render_template('index.html', **links)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    tf.logging.set_verbosity(tf.logging.INFO)
+
+    app.run(host='0.0.0.0',  # 任何ip都可以访问
+            port=7777,  # 端口
+            debug=True)
